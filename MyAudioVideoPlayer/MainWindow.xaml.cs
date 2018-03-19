56bb06b26339f26f5f255465ab14d922bfba8132
Jerry Blåfield <K9105@student.jamk.fi>
2018-03-19 16:52:56 +0200

    Add project files.

---
+++ MyAudioVideoPlayer/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyAudioVideoPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void IsPlaying (bool flag)
+        {
+            Play.IsEnabled = flag;
+            Stop.IsEnabled = flag;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Browse_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box 
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = "Videos"; // Default file name 
+            dialog.DefaultExt = ".WMV"; // Default file extension 
+            dialog.Filter = "WMV file (.wm)|*.wmv"; // Filter files by extension  
+
+            // Show open file dialog box 
+            Nullable<bool> result = dialog.ShowDialog();
+
+            // Process open file dialog box results  
+            if (result == true)
+            {
+                // Open document  
+                MePlayer.Source = new Uri(dialog.FileName);
+                Play.IsEnabled = true;
+            }
+            if (result == true)
+
+            {
+
+                // Open document
+
+                string filename = dialog.FileName;
+
+                Filepath.Text = filename;
+
+            }
+
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            IsPlaying(false);
+            MePlayer.Stop();
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            IsPlaying(true);
+            MePlayer.Play();
+        }
+
+
+    }
+}
---
+++ PasswordChecker/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PasswordChecker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void pwdBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            //password has changed, get stats from current pwd
+            int[] statistics = new int[5];
+
+            // char count
+            statistics[0] = pwdBox.Password.Length;
+
+            //caps
+            statistics[1] = pwdBox.Password.Count(char.IsUpper);
+
+            //small
+            statistics[2] = pwdBox.Password.Count(char.IsLower);
+
+            //numbers
+            statistics[3] = pwdBox.Password.Count(char.IsNumber);
+
+            //all the rest are special chars
+            statistics[4] = pwdBox.Password.Length - (statistics[1] + statistics[2] + statistics[3]);
+
+            //update the UI
+            txtCharCount.Text = "- Char count = " + statistics[0].ToString();
+            txtCapitalLetters.Text = "- Capital letters = " + statistics[1].ToString();
+            txtSmallLetters.Text = "- Small letters = " + statistics[2].ToString();
+            txtNumbers.Text = "- Numbers = " + statistics[3].ToString();
+            txtSpecialChars.Text = "- Special chars = " + statistics[4].ToString();
+
+            int categoryCount = 0;
+            for (int i=1; i<5; i++)
+            {
+                if (statistics[i] > 0)
+                {
+                    categoryCount++;
+                }
+            }
+
+            if (statistics[0] > 15 && categoryCount > 3)
+            {
+                // good pwd
+                txtCheckMessage.Content = "GOOD!";
+                txtCheckMessage.Background = new SolidColorBrush(Colors.Green);
+            }
+            else if (statistics[0] > 12 && categoryCount > 2)
+            {
+                //moderate pwd
+                txtCheckMessage.Content = "MODERATE!";
+                txtCheckMessage.Background = new SolidColorBrush(Colors.LightGreen);
+            }
+            else if (statistics[0] > 8 && categoryCount > 1)
+            {
+                //moderate pwd
+                txtCheckMessage.Content = "FAIR!";
+                txtCheckMessage.Background = new SolidColorBrush(Colors.Yellow);
+            }
+            else
+            {
+                //moderate pwd
+                txtCheckMessage.Content = "POOR!";
+                txtCheckMessage.Background = new SolidColorBrush(Colors.Red);
+            }
+        }
+    }
+}
